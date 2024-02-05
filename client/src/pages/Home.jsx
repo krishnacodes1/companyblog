795ca1a65d74bd75897103ef49c4342b258fbc24
syedufaq.haider <syedufaq.haider@w3villa.com>
2024-02-05 11:44:49 +0530

    made-react-changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -14,6 +14,8 @@ export default function Home() {
     };
     fetchPosts();
   }, []);
+
+  console.log(posts)
   return (
     <div>
       <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto '>
@@ -29,9 +31,9 @@ export default function Home() {
           View all posts
         </Link>
       </div>
-      <div className='p-3 bg-amber-100 dark:bg-slate-700'>
+      {/* <div className='p-3 bg-amber-100 dark:bg-slate-700'>
         <CallToAction />
-      </div>
+      </div> */}
 
       <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
         {posts && posts.length > 0 && (
