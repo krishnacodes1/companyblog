095571da023237fad268865e21660af9345a41e9
Krishna Sahu <55760144+krishnacodes1@users.noreply.github.com>
2024-02-18 19:27:34 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'blogs.velocitytech.in',
+        target: 'https://blogs.velocitytech.in',
         secure: false,
       },
     },
